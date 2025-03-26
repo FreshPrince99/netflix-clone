1bc8fe39b81229eb89145a9b4baa5a2361eb1ad7
Aaditya Nair <aaditya.nair2412@gmail.com>
2025-03-26 22:30:01 +0000

    created movie and tv show fetching functionality

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'; // For this we have to change the type field in the package.json file as module
 
 import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js';
+import tvRoutes from './routes/tv.route.js';
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -9,6 +11,8 @@ const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // this will allow us to parse req.body object in the controllers
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
 
 app.listen(PORT, () => {
     console.log("Server started at http://localhost:"+ PORT);
