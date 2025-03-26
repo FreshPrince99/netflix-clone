0912729bb40b380a065dc8f01ce6cf34d2681fb3
Aaditya Nair <aaditya.nair2412@gmail.com>
2025-03-26 22:51:14 +0000

    added middleware to protect routes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from 'express'; // For this we have to change the type field in the package.json file as module
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
 import tvRoutes from './routes/tv.route.js';
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
+import { protectRoute } from './middleware/protectRoute.js';
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // this will allow us to parse req.body object in the controllers
+app.use(cookieParser());
+
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
     console.log("Server started at http://localhost:"+ PORT);
